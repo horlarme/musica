e668e9daa68db73ba05ac77077c3073352a3375f
Lawal Oladipupo <lawaloladipupo@outlook.com>
2019-04-20 20:36:33 +0200

    Added guesture to album

--- lib/Components/Album.dart
+++ lib/Components/Album.dart
@@ -23,46 +23,54 @@ class Album extends StatelessWidget {
 
   @override
   build(BuildContext context) {
-    return Card(
-      color: Colors.transparent,
-      child: Column(
-        children: <Widget>[
-          Container(
-              margin: EdgeInsets.only(top: 15),
-              height: 90,
-              width: 90,
-              decoration: BoxDecoration(
-                  border: Border.all(width: 5, color: Colors.white),
-                  borderRadius: BorderRadius.circular(100),
-                  image: DecorationImage(
-                      image: AssetImage(this.artwork != null
-                          ? this.artwork
-                          : "assets/artworks/TalkmuzikDotCom.jpg")))),
-          Container(
-              margin: EdgeInsets.only(top: 10, bottom: 2, left: 10, right: 10),
-              child: Text(
-                this.title,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "AllerDisplay",
-                    letterSpacing: 1,
-                    color: Colors.white),
-              )),
-          Container(
-              margin: EdgeInsets.only(top: 0, bottom: 5, left: 10, right: 10),
-              child: Text(
-                this.artist,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 13,
-                    fontFamily: "AllerDisplay",
-                    letterSpacing: 1,
-                    color: backgroundColor.withOpacity(0.8)),
-              ))
-        ],
+    return GestureDetector(
+      onDoubleTap: (){
+        print("Double Tap");
+      },
+      onTap: (){
+        print('Tapped');
+      },
+      child: Card(
+        color: Colors.transparent,
+        child: Column(
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.only(top: 15),
+                height: 90,
+                width: 90,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 5, color: Colors.white),
+                    borderRadius: BorderRadius.circular(100),
+                    image: DecorationImage(
+                        image: AssetImage(this.artwork != null
+                            ? this.artwork
+                            : "assets/artworks/TalkmuzikDotCom.jpg")))),
+            Container(
+                margin: EdgeInsets.only(top: 10, bottom: 2, left: 10, right: 10),
+                child: Text(
+                  this.title,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "AllerDisplay",
+                      letterSpacing: 1,
+                      color: Colors.white),
+                )),
+            Container(
+                margin: EdgeInsets.only(top: 0, bottom: 5, left: 10, right: 10),
+                child: Text(
+                  this.artist,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontFamily: "AllerDisplay",
+                      letterSpacing: 1,
+                      color: backgroundColor.withOpacity(0.8)),
+                ))
+          ],
+        ),
       ),
     );
   }
