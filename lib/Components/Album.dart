0bd315246c2ad44d2da4671635b30c2f525622f0
Lawal Oladipupo Olawunmi <lawaloladipupo@outlook.com>
2019-04-22 03:54:57 +0200

    Changed and convert Album Art to component

--- lib/Components/Album.dart
+++ lib/Components/Album.dart
@@ -1,32 +1,21 @@
-import 'dart:io';
-
 import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:musicau/Components/SpinningArtwork.dart';
 import 'package:musicau/Config/colors.dart';
 
 class Album extends StatefulWidget {
   final Song song;
 
-  Album(this.song, {Key key});
+  bool playing;
+
+  Album(this.song, {Key key, this.playing = false});
 
   @override
   _Album createState() => _Album();
 }
 
-class _Album extends State<Album> with TickerProviderStateMixin {
-  AnimationController rotationController;
-
-  bool rotating = false;
-
-  @override
-  initState() {
-    rotationController = new AnimationController(
-        vsync: this, duration: Duration(milliseconds: 5000));
-
-    super.initState();
-  }
-
+class _Album extends State<Album> {
   @override
   build(BuildContext context) {
     return GestureDetector(
@@ -34,7 +23,11 @@ class _Album extends State<Album> with TickerProviderStateMixin {
         print("Double Tap");
       },
       onTap: () {
-        _toggleRotation();
+        print("Tapped");
+
+        setState(() {
+          widget.playing = !widget.playing;
+        });
       },
       child: Card(
         color: Colors.transparent,
@@ -42,29 +35,7 @@ class _Album extends State<Album> with TickerProviderStateMixin {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RotationTransition(
-                turns: Tween(begin: 0.0, end: 1.0).animate(rotationController),
-                child: Container(
-                    height: 90,
-                    width: 90,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 5, color: Colors.white),
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    child: Stack(
-                      fit: StackFit.expand,
-                        children: <Widget>[
-                      ClipOval(
-                          child: widget.song.albumArt != null
-                              ? Image.file(
-                                  File(widget.song.albumArt),
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.asset(
-                                  "assets/artworks/TalkmuzikDotCom.jpg",
-                                  fit: BoxFit.cover,
-                                )),
-                    ]))),
+            SpinningArtwork(widget.song, playing: widget.playing),
             Container(
                 margin: EdgeInsets.only(top: 10, bottom: 2, left: 5, right: 5),
                 child: Text(
@@ -91,23 +62,5 @@ class _Album extends State<Album> with TickerProviderStateMixin {
         ),
       ),
     );
-  }
-
-  void _toggleRotation() {
-    setState(() {
-      rotating = !rotating;
-
-      if (rotating) {
-        rotationController.stop();
-      } else {
-        rotationController.repeat();
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    rotationController.dispose();
-    super.dispose();
   }
 }
---
+++ lib/Components/SpinningArtwork.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flute_music_player/flute_music_player.dart';
+import 'package:flutter/material.dart';
+
+class SpinningArtwork extends StatefulWidget {
+  Song song;
+
+  bool playing = false;
+
+  SpinningArtwork(this.song, {Key key, this.playing}) : assert(song is Song);
+
+  @override
+  _SpinningArtwork createState() => _SpinningArtwork();
+}
+
+class _SpinningArtwork extends State<SpinningArtwork>
+    with TickerProviderStateMixin {
+  AnimationController rotationController;
+
+  @override
+  initState() {
+    rotationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 5000));
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(SpinningArtwork oldWidget) {
+    //decide on whether to spin or not
+    if (widget.playing) {
+      rotationController.repeat();
+    } else {
+      rotationController.stop();
+    }
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+        turns: Tween(begin: 0.0, end: 1.0).animate(rotationController),
+        child: Container(
+            height: 90,
+            width: 90,
+            decoration: BoxDecoration(
+              border: Border.all(width: 5, color: Colors.white),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Stack(fit: StackFit.expand, children: <Widget>[
+              ClipOval(
+                  child: widget.song.albumArt != null
+                      ? Image.file(
+                          File(widget.song.albumArt),
+                          fit: BoxFit.cover,
+                        )
+                      : Image.asset(
+                          "assets/artworks/TalkmuzikDotCom.jpg",
+                          fit: BoxFit.cover,
+                        )),
+            ])));
+  }
+
+  @override
+  void dispose() {
+    rotationController.dispose();
+    super.dispose();
+  }
+}
